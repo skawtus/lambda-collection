b0dc45c6b5a7f1fc69e0b2a9f8a38a6a592fae55
skawtus <skawtus@gmail.com>
2014-11-28 14:17:19 -0800

    basic pull mechanism from queue (delete message).

--- routes/pull.js
+++ routes/pull.js
@@ -3,8 +3,10 @@ var router = express.Router();
 var AWS = require('aws-sdk');
 var DEBUG = true;
 
+
+
 function notify_arrival(req,res,callback){
-    
+
     var sqs = new AWS.SQS({
         apiVersion: '2012-11-05',
         region: 'us-west-2'
@@ -15,54 +17,65 @@ function notify_arrival(req,res,callback){
         AttributeNames: [
             'All'
         ],
-        MaxNumberOfMessages: 0,
-        //MessageAttributeNames: [
-        //    'STRING_VALUE',
-        //    /* more items */
-        //],
+        MaxNumberOfMessages: 10,
         VisibilityTimeout: 0,
         WaitTimeSeconds: 0
     };
-    sqs.receiveMessage(params, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(data);           // successful response
+    sqs.receiveMessage(params,  function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            res.json([err,err.stack]);
+        }
+        else {
+            //console.log(data);
+            callback(req,res, data);
+        }
     });
-
-//
-//    var params = {
-//      Message: JSON.stringify({
-//          default: JSON.stringify({
-//              remote_address: req._remoteAddress,
-//              startTime: req._startTime,
-//              query: req.query,
-//              params: req.params,
-//              cookies: req.cookies,
-//              originalUrl: req.originalUrl
-//          })
-//
-//      }),
-//      MessageStructure: 'json',
-//      TopicArn: process.env.SNOWBALL_TOPIC
-//    };
-//    sns.publish(params, function(err, data) {
-//        if (err) {
-//            console.log(err, err.stack);
-//            res.json([err,err.stack]);
-//        }
-//        else {
-//            console.log(data);
-//            callback(req,res);
-//        }
-//    });
 
 }
 
-router.get('/pull', function(req, res) {
-    debugger;
-  //  notify_arrival(req,res,function(req,res){
-  //      if(DEBUG) res.json(["Ok"]);  //respond outside of handler for quickness
-  //  });
-  //  if(!DEBUG) res.json(["Ok"]);
+function handle_messages(req, res, data, callback){
+
+    _.each(data.Messages, function(message){
+        var c = JSON.parse(message.Body);
+        if( !("a" in c.query) || store(c)) {
+            delete_queue_message(message)
+        }
+    });
+
+    callback(req,res);
+}
+
+function store(content){
+    //debugger;
+    //TODO store this message...
+    return false;
+}
+
+function delete_queue_message(msg){
+
+    var sqs = new AWS.SQS({
+        apiVersion: '2012-11-05',
+        region: 'us-west-2'
+    });
+    var params = {
+        QueueUrl: process.env.SNOWBALL_QUEUE,
+        ReceiptHandle: msg.ReceiptHandle
+    };
+    sqs.deleteMessage(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else     console.log(data);           // successful response
+    });
+}
+
+router.get('/', function(req, res) {
+    notify_arrival(req,res,function(req,res,data){
+        handle_messages(req, res, data, function(req,res){
+
+            if(DEBUG) res.json(["Ok"]);  //respond outside of handler for quickness
+        });
+    });
+    if(!DEBUG) res.json(["Ok"]);
 });
 
 module.exports = router;
